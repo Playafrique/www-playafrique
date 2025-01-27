f7e812a5e0a75cb28e549e0cdf4b16b9274b2d41
Newton Mbugua <mymbugua@gmail.com>
2025-01-27 22:30:09 +0000

    feat: added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,8 @@
 import './globals.css'
-import '@fontsource-variable/dancing-script';
-import '@fontsource-variable/montserrat';
+import '@fontsource-variable/dancing-script'
+import '@fontsource-variable/montserrat'
 
 import type { Metadata } from 'next'
-import { Montserrat, Dancing_Script } from 'next/font/google'
-
-const dsfont = Dancing_Script({ subsets: ['latin'], display:'swap', variable: '--font-dancing-script' })
-const font = Montserrat({ subsets: ['latin'], display:'swap', variable: '--font-montserrat' })
 
 export const metadata: Metadata = {
     title: 'Play Afrique',
@@ -20,9 +16,26 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
     return (
         <html lang='en-GB'>
-            <body className='font-sans'>
-                {children}
-            </body>
+            <head>
+                <meta charSet='utf-8' />
+                <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1'
+                />
+                <meta
+                    name='keywords'
+                    content='Events, Play Afrique, African Events, UK African Events'
+                />
+
+                <link
+                    rel='icon'
+                    href='/favicon-32x32.png'
+                    type='image/png'
+                    sizes='32x32'
+                />
+            </head>
+            <body className='font-sans'>{children}</body>
         </html>
     )
 }
